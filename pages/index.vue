c1e82e8e0d251a5e6de1a34abe23a9ace1775d7e
evermake <vladeryabkin@gmail.com>
2023-06-22 19:11:44 +0300

    refactor!: migrate project to Nuxt.js

---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Welcome to room booking!</h1>
+  <Map></Map>
+</template>
--- src/components/Map/index.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./styles.module.css";
-
-export default function Index() {
-  return <div className={styles.map}>This is map</div>;
-}
