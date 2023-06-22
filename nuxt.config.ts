f70ce82324e95d72ce17edf860aa559e4bd7e330
evermake <vladeryabkin@gmail.com>
2023-06-22 22:23:43 +0300

    feat: begin creating main page UI, fix styles formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["~/assets/css/globals.css"],
+  modules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    subsets: ["cyrillic", "latin"],
+    families: {
+      "IBM+Plex+Sans": [400],
+      "IBM+Plex+Mono": [400],
+    },
+  },
+});
