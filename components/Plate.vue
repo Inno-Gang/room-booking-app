9787cf6d92c9f4474680c71b7872fc3812ec0140
Vladislav Deryabkin <53311479+evermake@users.noreply.github.com>
2023-06-23 18:05:38 +0300

    feat: split main page into components, add tab switching (#8)

---
+++ components/Plate.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="plate">
+    <slot/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$borders-color: #D4D4D4;
+
+.plate {
+  border: 1px solid $borders-color;
+  border-radius: 4px;
+}
+
+</style>
